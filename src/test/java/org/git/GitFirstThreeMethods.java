46e23e3fd9ee9982b6c1a1527dd571cbf8018c65
UshaMurugan07 <ushamurugan66@gmail.com>
2023-03-12 14:55:01 +0530

    Added 3 methods in India

--- src/test/java/org/git/GitFirstThreeMethods.java
+++ src/test/java/org/git/GitFirstThreeMethods.java
@@ -10,5 +10,13 @@ System.out.println("Dhana");
 	private void test3() {
 System.out.println("Revathi");
 	}
-
+private void test4() {
+System.out.println("jaya");
+}
+private void test5() {
+	System.out.println("java");
+}
+private void test6() {
+System.out.println("python");
+}
 }
