c1e84d227e4c117b8b25e050da2a96df8e9a8d4c
UshaMurugan07 <ushamurugan66@gmail.com>
2023-03-12 11:16:43 +0530

    intial 3 methods of git master

---
+++ src/test/java/org/git/GitFirstThreeMethods.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class GitFirstThreeMethods {
+	private void test1() {
+System.out.println("usha");
+	}
+	private void test2() {
+System.out.println("Dhana");
+	}
+	private void test3() {
+System.out.println("Revathi");
+	}
+
+}
